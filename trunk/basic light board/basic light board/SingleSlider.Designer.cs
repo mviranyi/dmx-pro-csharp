1565884a7da6700e98ed8a313b4d128da8e08de6
bveina <bveina@dcc82a8e-f49f-11de-a89a-43a960051dd3>
2010-01-17 04:51:42 +0000

    this is a working prototype that was actually used for a performance of death of a salesman. im pretty proud of this atm.

--- trunk/basic light board/basic light board/SingleSlider.Designer.cs
+++ trunk/basic light board/basic light board/SingleSlider.Designer.cs
@@ -75,9 +75,10 @@
             // 
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(0, 0);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(31, 20);
+            this.textBox1.Size = new System.Drawing.Size(31, 17);
             this.textBox1.TabIndex = 4;
             this.textBox1.TabStop = false;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
